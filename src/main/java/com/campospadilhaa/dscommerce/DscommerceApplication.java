425a3fef0d1b10357863e89224e34ccf2c9fd94a
Anderson Campos Padilha <campospadilha.a@gmail.com>
2025-02-10 08:32:48 -0300

    Primeiro commit, criação do projeto

---
+++ src/main/java/com/campospadilhaa/dscommerce/DscommerceApplication.java
@@ -0,0 +1,12 @@
+package com.campospadilhaa.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+}
