5fffaaaf566f2a8d4d39e04aca4015a55a670ee1
Anderson Campos Padilha <campospadilha.a@gmail.com>
2025-02-11 09:01:47 -0300

    Criação do bean Order e enum OrderStatus

--- src/main/java/com/campospadilhaa/dscommerce/entities/User.java
+++ src/main/java/com/campospadilhaa/dscommerce/entities/User.java
@@ -1,12 +1,15 @@
 package com.campospadilhaa.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ public class User {
 	private String phone;
 	private LocalDate birthDate;
 	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 
@@ -83,6 +89,14 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 
 	@Override
