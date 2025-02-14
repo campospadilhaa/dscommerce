6aa8db80170ace150e996927a59e58e59d4f3178
Anderson Campos Padilha <campospadilha.a@gmail.com>
2025-02-14 09:43:25 -0300

    Criação do Repository 'ProductRepository' e testado o primeiro acesso ao banco

--- src/main/java/com/campospadilhaa/dscommerce/controllers/ProductController.java
+++ src/main/java/com/campospadilhaa/dscommerce/controllers/ProductController.java
@@ -1,15 +1,29 @@
 package com.campospadilhaa.dscommerce.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.campospadilhaa.dscommerce.entities.Product;
+import com.campospadilhaa.dscommerce.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@GetMapping
 	public String teste() {
-		return "Olá mundo!";
+
+		Optional<Product> productOptional = productRepository.findById(1L);
+
+		Product product = productOptional.get();
+
+		return product.getName();
 	}
 }
