720a2d62d3109ceadde76003deddc87ded1de376
Anderson Campos Padilha <campospadilha.a@gmail.com>
2025-03-23 20:49:24 -0300

    Criada a classe: 'AuthorizationServerConfig' e as auxiliares: 'customgrant' que realizarão a autenticação utilizando OAuth2

---
+++ src/main/java/com/campospadilhaa/dscommerce/config/AuthorizationServerConfig.java
@@ -0,0 +1,226 @@
+package com.campospadilhaa.dscommerce.config;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.OAuth2Token;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.DelegatingOAuth2TokenGenerator;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.JwtGenerator;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2AccessTokenGenerator;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.campospadilhaa.dscommerce.config.customgrant.CustomPasswordAuthenticationConverter;
+import com.campospadilhaa.dscommerce.config.customgrant.CustomPasswordAuthenticationProvider;
+import com.campospadilhaa.dscommerce.config.customgrant.CustomUserAuthorities;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+// Classe que configura as permissões a nível do servidor de aplicação;
+
+@Configuration
+public class AuthorizationServerConfig {
+
+	// variáveis de ambiente obtidas do arquivo: application.properties, sendo:
+	// * client-id; * client-secret; e * jwt.duration
+
+	@Value("${security.client-id}")
+	private String clientId;
+
+	@Value("${security.client-secret}")
+	private String clientSecret;
+
+	@Value("${security.jwt.duration}")
+	private Integer jwtDurationSeconds;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	// utilização das classes auxiliares: CustomPasswordAuthenticationConverter; CustomPasswordAuthenticationProvider; CustomPasswordAuthenticationToken; CustomUserAuthorities
+	// para customizar a GrantTypePassword
+
+	@Bean
+	@Order(2) // os order: 1 e 3, estão no ResourceServerConfig.java
+	public SecurityFilterChain asSecurityFilterChain(HttpSecurity http) throws Exception {
+
+		// integrando o Spring Security com o OAuth2
+		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+		// @formatter:off
+		http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+			.tokenEndpoint(tokenEndpoint -> tokenEndpoint
+
+				// customização do token
+				.accessTokenRequestConverter(new CustomPasswordAuthenticationConverter())
+
+				// configuração da autenticação, leitura das credenciais. Configurando o Spring Security com OAuth2 
+				// utilizando o 'UserDetailsService.java' que criamos para buscar o usuário passando o nome do usuário (username)
+				.authenticationProvider(new CustomPasswordAuthenticationProvider(authorizationService(), tokenGenerator(), userDetailsService, passwordEncoder())));
+
+		// configuração para trabalhar com o token JWT
+		http.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
+		// @formatter:on
+
+		return http.build();
+	}
+
+	@Bean
+	public OAuth2AuthorizationService authorizationService() {
+		return new InMemoryOAuth2AuthorizationService();
+	}
+
+	@Bean
+	public OAuth2AuthorizationConsentService oAuth2AuthorizationConsentService() {
+		return new InMemoryOAuth2AuthorizationConsentService();
+	}
+
+	// criação do componente para criptografar a senha do usuário
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	// registro do cliente autorizado a utilizar a aplicação
+	@Bean
+	public RegisteredClientRepository registeredClientRepository() {
+		// @formatter:off
+		RegisteredClient registeredClient = RegisteredClient
+			.withId(UUID.randomUUID().toString())
+			.clientId(clientId)
+			.clientSecret(passwordEncoder().encode(clientSecret))
+			.scope("read")
+			.scope("write")
+			.authorizationGrantType(new AuthorizationGrantType("password"))
+			.tokenSettings(tokenSettings())
+			.clientSettings(clientSettings())
+			.build();
+		// @formatter:on
+
+		// registro do objeto em memória
+		return new InMemoryRegisteredClientRepository(registeredClient);
+	}
+
+	@Bean
+	public TokenSettings tokenSettings() {
+		// @formatter:off
+		return TokenSettings.builder()
+			.accessTokenFormat(OAuth2TokenFormat.SELF_CONTAINED)
+
+			// configuração do tempo de duração o token obtido do application.properties: ${security.jwt.duration}
+			.accessTokenTimeToLive(Duration.ofSeconds(jwtDurationSeconds))
+			.build();
+		// @formatter:on
+	}
+
+	@Bean
+	public ClientSettings clientSettings() {
+		return ClientSettings.builder().build();
+	}
+
+	@Bean
+	public AuthorizationServerSettings authorizationServerSettings() {
+		return AuthorizationServerSettings.builder().build();
+	}
+
+	@Bean
+	public OAuth2TokenGenerator<? extends OAuth2Token> tokenGenerator() {
+		NimbusJwtEncoder jwtEncoder = new NimbusJwtEncoder(jwkSource());
+		JwtGenerator jwtGenerator = new JwtGenerator(jwtEncoder);
+		jwtGenerator.setJwtCustomizer(tokenCustomizer());
+		OAuth2AccessTokenGenerator accessTokenGenerator = new OAuth2AccessTokenGenerator();
+		return new DelegatingOAuth2TokenGenerator(jwtGenerator, accessTokenGenerator);
+	}
+
+	// customizador do token
+	// 
+	@Bean
+	public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+		return context -> {
+			OAuth2ClientAuthenticationToken principal = context.getPrincipal();
+			CustomUserAuthorities user = (CustomUserAuthorities) principal.getDetails();
+			List<String> authorities = user.getAuthorities().stream().map(x -> x.getAuthority()).toList();
+			if (context.getTokenType().getValue().equals("access_token")) {
+				// @formatter:off
+
+				// configuração das reivindicações (claim's)
+				// que estarão contidas no corpo do 'PAYLOAD' do token
+				context.getClaims()
+					.claim("authorities", authorities)
+					.claim("username", user.getUsername());
+
+				// @formatter:on
+			}
+		};
+	}
+
+	// decodificador do token
+	@Bean
+	public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+		return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+	}
+
+	// sequencia de 3 (três) métodos que realizam a assinatura do token
+	// criação da chave RSA que o meu AuthorizaginServer reconhecerá como uma chave válida
+	@Bean
+	public JWKSource<SecurityContext> jwkSource() {
+		RSAKey rsaKey = generateRsa();
+		JWKSet jwkSet = new JWKSet(rsaKey);
+		return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+	}
+
+	private static RSAKey generateRsa() {
+		KeyPair keyPair = generateRsaKey();
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		return new RSAKey.Builder(publicKey).privateKey(privateKey).keyID(UUID.randomUUID().toString()).build();
+	}
+
+	private static KeyPair generateRsaKey() {
+		KeyPair keyPair;
+		try {
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			keyPair = keyPairGenerator.generateKeyPair();
+		} catch (Exception ex) {
+			throw new IllegalStateException(ex);
+		}
+		return keyPair;
+	}
+	////
+}
